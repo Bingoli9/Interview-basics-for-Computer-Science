2be0e555804c4909bc77da3b485ca0ae18d419e4
leekeiling <2332433591@qq.com>
2019-03-07 12:17:26 +0800

    up

---
+++ Util/String/READMEl.md
@@ -0,0 +1,6 @@
+### StringUtil
+
+- split string
+
+
+
---
+++ Util/String/StringUtil.hpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <stdio.h>
+#include <string.h>
+
+
+class StringUtil
+{
+	StringUtil() = default;
+	~StringUtil() = default;
+	
+	void strSplit(const std::string& s, std::vector<std::string>& v, const std::string& sep)
+	{
+		std::string::size_type pos1, pos2;
+		pos2 = s.find(sep);
+		pos1 = 0;
+		while(std::string::npos != pos2)
+		{
+			v.push_back(s.substr(pos1, pos2-pos1));
+			
+			pos1 = pos2 + sep.size();
+			pos2 = s.find(sep, pos1);
+		}
+		if (pos1 !=s.length()) {
+			v.push_back(s.substr(pos1));
+		}
+	}
+	
+	void strSplit(char* s, std::vector<std::string>& v, const char* sep)
+	{
+		char *p;
+		p = strtok(s, sep);
+		while(p) {
+			v.push_back(p);
+			p = strtok(NULL, sep);
+		}
+	}
+};
